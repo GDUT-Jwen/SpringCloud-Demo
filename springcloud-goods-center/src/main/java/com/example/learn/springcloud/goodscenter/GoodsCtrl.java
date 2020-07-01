1fa258c42925ed59e8cf9c77102c88c4b330d202
jwen <452321360@qq.com>
2020-07-01 17:20:06 +0800

    undertow && hutools

--- springcloud-goods-center/src/main/java/com/example/learn/springcloud/goodscenter/GoodsCtrl.java
+++ springcloud-goods-center/src/main/java/com/example/learn/springcloud/goodscenter/GoodsCtrl.java
@@ -1,7 +1,15 @@
 package com.example.learn.springcloud.goodscenter;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.watch.WatchMonitor;
+import cn.hutool.core.io.watch.Watcher;
+import cn.hutool.core.lang.Console;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
 
 @RestController
 public class GoodsCtrl {
@@ -9,6 +17,44 @@ public class GoodsCtrl {
     @GetMapping("/getMsg")
     public String returnGoodsName() {
         return "this is an apple!";
+    }
+
+    @GetMapping("/watchFiles")
+    public String watchFiles() {
+        File file = FileUtil.file("G://");
+        //这里只监听文件或目录的修改事件
+        WatchMonitor watchMonitor = WatchMonitor.create(file, WatchMonitor.EVENTS_ALL);
+        watchMonitor.setWatcher(new Watcher() {
+            @Override
+            public void onCreate(WatchEvent<?> event, Path currentPath) {
+                Object obj = event.context();
+                Console.log("创建：{}-> {}", currentPath, obj);
+            }
+
+            @Override
+            public void onModify(WatchEvent<?> event, Path currentPath) {
+                Object obj = event.context();
+                Console.log("修改：{}-> {}", currentPath, obj);
+            }
+
+            @Override
+            public void onDelete(WatchEvent<?> event, Path currentPath) {
+                Object obj = event.context();
+                Console.log("删除：{}-> {}", currentPath, obj);
+            }
+
+            @Override
+            public void onOverflow(WatchEvent<?> event, Path currentPath) {
+                Object obj = event.context();
+                Console.log("Overflow：{}-> {}", currentPath, obj);
+            }
+        });
+
+        //设置监听目录的最大深入，目录层级大于制定层级的变更将不被监听，默认只监听当前层级目录
+        watchMonitor.setMaxDepth(3);
+        //启动监听
+        watchMonitor.start();
+        return "watchFiles";
     }
 
 }
